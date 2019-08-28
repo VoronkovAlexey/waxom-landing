4195b3a140d75a105868ece06d5b074646e1b7bb
Alex Voronkov <lexavoronkov007@yandex.by>
2019-08-28 23:06:44 +0300

    Сверстана вторая страница

--- js/script.js
+++ js/script.js
@@ -22,4 +22,55 @@ function rightarrow(){
     // document.write()
     
 }
-    
+
+function Block1ImgHover(){
+    document.getElementById("block1Img").src = "img/icons/icon_1_1.png";
+    document.getElementById("line1").src = "img/icons/line_1_1.png";
+
+}
+
+function Block1Img(){
+    document.getElementById("block1Img").src = "img/icons/icon_1.png";
+    document.getElementById("line1").src = "img/icons/line_1.png";
+
+}
+
+function Block2ImgHover(){
+    document.getElementById("block2Img").src = "img/icons/icon_2_1.png";
+    document.getElementById("line2").src = "img/icons/line_1_1.png";
+
+}
+
+function Block2Img(){
+    document.getElementById("block2Img").src = "img/icons/icon_2.png";
+    document.getElementById("line2").src = "img/icons/line_1.png";
+
+}
+
+function Block3ImgHover(){
+    document.getElementById("block3Img").src = "img/icons/icon_3_1.png";
+    document.getElementById("line3").src = "img/icons/line_1_1.png";
+
+}
+
+function Block3Img(){
+    document.getElementById("block3Img").src = "img/icons/icon_3.png";
+    document.getElementById("line3").src = "img/icons/line_1.png";
+
+}
+
+function Block4ImgHover(){
+    document.getElementById("block4Img").src = "img/icons/icon_4_1.png";
+    document.getElementById("line4").src = "img/icons/line_1_1.png";
+
+}
+
+function Block4Img(){
+    document.getElementById("block4Img").src = "img/icons/icon_4.png";
+    document.getElementById("line4").src = "img/icons/line_1.png";
+
+}
+
+
+
+
